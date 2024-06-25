a2e0d00754d613ed10068c87b5d840aead919ae9
Grant <gsteere4717@xmail.dixietech.edu>
2024-06-25 09:05:54 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Vite
+
+THis is where I test my various react code solutions and what not.  I'm learning cut some slack please.
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <>
+    <h1>Hello Trena!</h1>
+    </>
+  )
+}
+
+export default App
